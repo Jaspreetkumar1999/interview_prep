90c49f70afebe96e1fda7897da010606ba53123e
jaspreet <jaspreetkumar0315@gmail.com>
2022-09-15 22:04:30 +0530

    implemented radix sort

---
+++ insertionSort.js
@@ -0,0 +1,9 @@
+var msg = "hi"
+
+function insertionSort(array){
+var msg = "hello"
+console.log(msg)
+}
+insertionSort([])
+console.log(msg)
+// console.log(insertionSort([]))insertionSort([])insertionSort([])
